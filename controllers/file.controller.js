4813214901cf8f037811873bea32677c8985e728
Marian Lyzhychka <mr.lezhichka@gmail.com>
2023-01-10 14:11:27 +0200

    added cors

--- controllers/file.controller.js
+++ controllers/file.controller.js
@@ -23,6 +23,9 @@ module.exports = {
                     responseWrapper.push(resp)
 
 
+                    res.setHeader('Access-Control-Allow-Origin', '*');
+                    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
+                    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
                     res.status(200).json(responseWrapper);
                 })
             })
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const app = express()
+const cors = require('cors');
+
 const port = 3000
 
 const {sharepointRouter} = require('./routes')
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+app.use(cors());
 
 
 app.use('/sharepoint', sharepointRouter);
